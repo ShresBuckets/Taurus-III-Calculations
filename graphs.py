163dfbd683f919f532e7f6d4760994414c1bac12
Neal Shrestha <71297631+ShresBuckets@users.noreply.github.com>
2025-11-02 18:03:39 +0000

    added more graphs

--- graphs.py
+++ graphs.py
@@ -11,10 +11,21 @@ Expansion Ratio v.s. OF
 """
 OF = df450["OF_RATIO"].to_numpy()
 ISP = df450["ISP"].to_numpy()
-plt.plot(OF,ISP)
-plt.xlabel("OF Ratio")
-plt.ylabel("ISP")
-plt.title("ISP v.s. OF at 450 psi")
+TEMP = df450["CHAMBER_TEMP"].to_numpy()
+
+fig, axs = plt.subplots(2)
+axs[0].plot(OF,ISP)
+axs[0].set_title("ISP v.s. OF at 450 psi")
+axs[0].set_xlabel("OF Ratio")
+axs[0].set_ylabel("ISP")
+
+axs[1].plot(OF,TEMP)
+axs[1].set_xlabel("OF Ratio")
+axs[1].set_ylabel("Chamber Temperature (K)")
+axs[1].set_title("Chamber Temp (K) v.s. OF at 450 psi")
+#axs[0].xlabel("OF Ratio")
+#axs[0].ylabel("ISP")
+#axs[0].title(f"ISP v.s. OF at {450} psi")
 plt.savefig("example.png")
 print(matplotlib.get_backend())
 
