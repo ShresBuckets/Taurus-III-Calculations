75c619f7a12cd0fe0692d77f1bd27b665d2a625f
Neal Shrestha <71297631+ShresBuckets@users.noreply.github.com>
2025-11-02 03:00:16 +0000

    code change

--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 df450 = pd.read_csv("data/450psi.csv")
 
 """
@@ -8,4 +9,6 @@ Temperature v.s. OF,
 Expansion Ratio v.s. OF
 """
 OF = df450["OF_RATIO"].to_numpy()
-print("works")
+ISP = df450["ISP"].to_numpy()
+plt.plot(OF,ISP)
+
