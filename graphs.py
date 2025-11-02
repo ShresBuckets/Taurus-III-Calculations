20cec23dcadadc43a4af7a114c1eb55690b458b1
Neal Shrestha <71297631+ShresBuckets@users.noreply.github.com>
2025-11-02 02:31:20 +0000

    updated data

--- graphs.py
+++ graphs.py
@@ -1,3 +1,11 @@
 import pandas as pd
+import numpy as np
 df450 = pd.read_csv("data/450psi.csv")
-print("worked")
+
+"""
+Graphs for ISP v.s. OF, 
+Temperature v.s. OF, 
+Expansion Ratio v.s. OF
+"""
+OF = df450["OF_RATIO"].to_numpy()
+print("works")
