1e200581d433d47d83eb9312dcc4343e7f39a3ac
Neal Shrestha <71297631+ShresBuckets@users.noreply.github.com>
2025-11-02 03:18:18 +0000

    fixed plot

--- graphs.py
+++ graphs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 df450 = pd.read_csv("data/450psi.csv")
 
 """
@@ -11,5 +12,9 @@ Expansion Ratio v.s. OF
 OF = df450["OF_RATIO"].to_numpy()
 ISP = df450["ISP"].to_numpy()
 plt.plot(OF,ISP)
-plt.show()
+plt.xlabel("OF Ratio")
+plt.ylabel("ISP")
+plt.title("ISP v.s. OF at 450 psi")
+plt.savefig("example.png")
+print(matplotlib.get_backend())
 
